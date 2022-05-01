c1eda0f1598020c7398a5fce2a08f3061135bcb6
ehsan <ehsansaraee6570@yahoo.com>
2022-05-01 13:30:59 +0430

    creating heroBanner, sanity client, showing first product on the screen

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -1,8 +1,39 @@
-const HeroBanner = () => {
+import { urlFor } from 'lib/client';
+import Link from 'next/link';
+
+const HeroBanner = ({
+   heroBanner: {
+      smallText,
+      midText,
+      largeText1,
+      image,
+      product,
+      buttonText,
+      desc,
+   },
+}) => {
    return (
-      <>
-         <h1>HeroBanner</h1>
-      </>
+      <section className="hero-banner-container">
+         <div>
+            <p className="beats-solo">{smallText}</p>
+            <h3>{midText}</h3>
+            <h1>{largeText1}</h1>
+            <img
+               src={urlFor(image)}
+               alt="headphones"
+               className="hero-banner-image"
+            />
+            <div>
+               <Link href={`/product/${product}`} passHref>
+                  <button type="button">{buttonText}</button>
+               </Link>
+               <div className="desc">
+                  <h5>Description</h5>
+                  <p>{desc}</p>
+               </div>
+            </div>
+         </div>
+      </section>
    );
 };
 
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,32 @@
-const Home = () => {
+import { FooterBanner, HeroBanner } from 'components';
+import { client } from 'lib/client';
+
+const Home = ({ products, bannerData }) => {
    return (
-      <>
-         <h1>Griffin Tech</h1>
-      </>
+      <section>
+         <HeroBanner heroBanner={bannerData.length && bannerData[0]} />
+         <div className="products-heading">
+            <h2>Best Seller Products</h2>
+            <p>speaker There are many variations and passages</p>
+         </div>
+         <div className="products-container">
+            {products?.map((product) => product.name)}
+         </div>
+         <FooterBanner />
+      </section>
    );
+};
+
+export const getServerSideProps = async () => {
+   const productsQuery = '*[_type == "product"]';
+   const products = await client.fetch(productsQuery);
+
+   const bannerQuery = '*[_type == "banner"]';
+   const bannerData = await client.fetch(bannerQuery);
+
+   return {
+      props: { products, bannerData },
+   };
 };
 
 export default Home;
